44634fab46ccf4ea1f6eeb33bcb62e463fcd4c9d
chrisb <chris@elf02.de>
2023-03-12 19:32:09 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,97 @@
+import './style.css'
+import Alpine from 'alpinejs'
+
+window.Alpine = Alpine;
+
+Alpine.data('braintrain', () => ({
+  cols: 0,
+  rows: 0,
+  tiles: [],
+  targetTilesCount: 0,
+  hint: false,
+  unsolvedHint: false,
+  selectionLocked: true,
+  solved: 0,
+  brainChecks: 0,
+  showMenu: true,
+
+  init() {
+    const tileWidth =  Math.floor(this.$refs.gridWrap.clientWidth / 4);
+    this.cols = Math.floor(this.$refs.gridWrap.clientWidth / tileWidth);
+    this.rows = Math.floor(this.$refs.gridWrap.clientHeight / tileWidth);
+  },
+
+  startBrainChecks() {
+    this.$refs.audioClick.play();
+    this.showMenu = false;
+    this.solved = 0;
+    this.brainChecks = 0;
+    setTimeout(() => this.initTiles(), 500);
+  },
+
+  initTiles() {
+    const tilesCount = this.cols * this.rows;
+    this.targetTilesCount = this.rndMinMax(2, 8);
+    this.tiles = [
+      ...Array.from({ length: this.targetTilesCount }, () => ({
+        isTarget: true,
+        isSelected: false,
+      })),
+      ...Array.from({ length: tilesCount - this.targetTilesCount }, () => ({
+        isTarget: false,
+        isSelected: false,
+      })),
+    ].sort((a, b) => 0.5 - Math.random());
+
+    this.unsolvedHint = false;
+    this.hint = true;
+    setTimeout(() => {
+      this.hint = false
+      this.selectionLocked = false;
+    }, 2000);
+  },
+
+  checkTiles() {
+    if (this.selectedTiles.length === this.targetTilesCount) {
+      this.brainChecks++;
+
+      if (this.targetTiles.length === this.targetTilesCount) {
+        this.$refs.audioSolved.play();
+        this.solved++;
+        this.selectionLocked = true;
+        setTimeout(() => this.initTiles(), 1000);
+      }
+      else {
+        this.selectionLocked = true;
+        this.unsolvedHint = true;
+        setTimeout(() => this.initTiles(), 1000);
+      }
+    }
+  },
+
+  selectTile(tile) {
+    if (this.selectionLocked) return;
+
+    this.$refs.audioClick.play();
+    tile.isSelected = true;
+    this.checkTiles();
+  },
+
+  rndMinMax(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  },
+
+  get selectedTiles() {
+    return this.tiles.filter(tile => tile.isSelected);
+  },
+
+  get targetTiles() {
+    return this.selectedTiles.filter(tile => tile.isTarget);
+  },
+
+  get tilesToSelect() {
+    return this.targetTilesCount - this.selectedTiles.length;
+  },
+}));
+
+Alpine.start();
---
+++ readme.md
@@ -0,0 +1,6 @@
+# Braintrain
+
+Simple tile memorization game.
+Build for fun with [Alpine.js](https://alpinejs.dev/) and [Tailwind CSS](https://tailwindcss.com/). Bundled with [Vite.js](https://vitejs.dev/)
+
+[Train your Brain!](https://elf02.github.io/braintrain/)
